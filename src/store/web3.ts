81681603848e76e720de0a06d155f69bf956ce7c
pandasoft6 <pym.4wd@outlook.com>
2022-04-05 08:56:20 -0400

    refactor

--- src/store/web3.ts
+++ src/store/web3.ts
@@ -166,10 +166,10 @@ export const actions: ActionTree<LocalState, RootState> = {
 			console.error(e)
 			const connectingWalletError = walletId === "metamask" && !window.ethereum?.isMetaMask ? "metamaskConnectionError" : "requestRejected"
 			commit("web3/connectingWalletError", connectingWalletError, { root: true })
-			window.ethereum?.on("connect", connectInfo => {
-				commit("web3/connectingWalletId", null)
-				commit("web3/connectingWalletError", null)
-			})
+			// window.ethereum?.on("connect", connectInfo => {
+			// 	commit("web3/connectingWalletId", null)
+			// 	commit("web3/connectingWalletError", null)
+			// })
 			dispatch("disconnect")
 		}
 	},
